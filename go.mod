fd7bc345a63440116301e2933858406d312eee75
Hamasah Fatiy Dakhilullah <hamasfa@gmail.com>
2025-03-29 22:18:57 +0700

    Add Gorilla Mux for routing and update main application to use it (Test using go mod download)

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module learn-ci-cd
 
 go 1.23.2
+
+require github.com/gorilla/mux v1.8.1
--- main.go
+++ main.go
@@ -5,16 +5,24 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	timeZone := time.FixedZone("UTC+7", 8*60*60)
 	timeUp := time.Now().In(timeZone).Format("2006-01-02 15:04:05")
 
-	http.HandleFunc("/", handler.IndexHandler())
-	http.HandleFunc("/health", handler.HealthHandler(timeUp, timeZone))
+	route := mux.NewRouter()
+
+	// http.HandleFunc("/", handler.IndexHandler())
+	// http.HandleFunc("/health", handler.HealthHandler(timeUp, timeZone))
+
+	route.HandleFunc("/", handler.IndexHandler())
+	route.HandleFunc("/health", handler.HealthHandler(timeUp, timeZone))
+
 	log.Println("Starting server on port 8080...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":8080", route); err != nil {
 		log.Fatal(err)
 	}
 }
